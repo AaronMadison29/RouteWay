d3679a72ad0c4d9c4804a0995613e7d2ab99f035
Aaron Madison <aaronmadison29@gmail.com>
2020-03-17 12:02:09 -0500

    Init

---
+++ Repository/Contracts/IRepositoryWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Contracts
+{
+    public interface IRepositoryWrapper
+    {
+        //interfaces go here
+        void Save();
+    }
+}
---
+++ Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
